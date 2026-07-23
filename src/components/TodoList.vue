06041c76d4a2982d46a3154bb320d2992a0ce4b9
Christian <caascaas@gmail.com>
2026-07-23 12:38:05 +0200

    restore Counter.vue, add shadcdn for UI, tailwind, add ol todolist

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,12 @@
 <template>
 	<h1>TodoList</h1>
+	<ol>
+		<li>Task 1</li>
+		<li>Task 2</li>
+		<li>Task 3</li>
+		<li>Task 4</li>
+		<li>Task 5</li>
+	</ol>
 </template>
 
 <script setup lang="ts">
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import vue from '@vitejs/plugin-vue';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [ vue() ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [vue(), tailwindcss()],
 });
