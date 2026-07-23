1c48141bc5121a83dace0401ba5bf9e8f5539555
Christian <caascaas@gmail.com>
2026-07-23 11:58:31 +0200

    Clean unnecessary code and add TodoList and composable

---
+++ src/components/TodoList.vue
@@ -0,0 +1,16 @@
+<template>
+	<h1>TodoList</h1>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+</script>
+
+<style scoped lang="scss">
+h1 {
+	font-size: 2em;
+	margin-bottom: 10px;
+}
+
+</style>
