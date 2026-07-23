b1f677a200f2cb9cbab3b7eced21187170f75a9e
Christian <caascaas@gmail.com>
2026-07-23 14:08:47 +0200

    add architectures data, update TodoList to render from JSON, add Todo type

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,23 +1,19 @@
+<script setup lang="ts">
+import architectures from '../data/architectures.json';
+</script>
+
 <template>
 	<h1>TodoList</h1>
 	<ol>
-		<li>Task 1</li>
-		<li>Task 2</li>
-		<li>Task 3</li>
-		<li>Task 4</li>
-		<li>Task 5</li>
+		<li v-for="architecture in architectures" :key="architecture.id">
+			{{ architecture.name }}
+		</li>
 	</ol>
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue';
-
-</script>
 
 <style scoped lang="scss">
 h1 {
 	font-size: 2em;
 	margin-bottom: 10px;
 }
-
 </style>
---
+++ src/types/types.ts
@@ -0,0 +1,8 @@
+type Todo = {
+    id: number;
+    name: string;
+    description: string;
+    completed: boolean;
+}
+
+export default Todo;
