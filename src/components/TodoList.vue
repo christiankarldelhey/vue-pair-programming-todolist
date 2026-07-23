be551e9680bb25a83519f44aed5209df647b141d
Christian <caascaas@gmail.com>
2026-07-23 18:11:20 +0200

    refactor search filter layout to use flex container, change reset button to outline variant and disable when search is empty

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -13,38 +13,32 @@ const { filteredTasks, searchQuery, resetSearch } = useFilter(tasks as Task[]);
 <template>
 	<h1 class="text-3xl font-bold mb-4">Todo-List</h1>
 	<div class="container">
-		<div class="relative">
-	<Input v-model="searchQuery" placeholder="Search tasks..." class="pr-20" />
-	<Button
-		v-if="searchQuery"
-		variant="destructive"
-		size="sm"
-		class="absolute right-1 top-1/2 -translate-y-1/2 h-7"
-		@click="resetSearch"
-	>
-		Reset
-	</Button>
-</div>
-	<Table class="mt-4">
-		<TableHeader>
-			<TableRow>
-				<TableHead>Task</TableHead>
-				<TableHead>Description</TableHead>
-				<TableHead>Completed</TableHead>
-			</TableRow>
-		</TableHeader>
-		<TableBody>
-			<TableRow v-if="filteredTasks.length === 0">
-				<TableCell colspan="3" class="text-center">No results</TableCell>
-			</TableRow>
-			<TableRow v-for="task in filteredTasks as Task[]" :key="task.id">
-				<TableCell>{{ task.name }}</TableCell>
-				<TableCell>{{ task.description }}</TableCell>
-				<TableCell>{{ task.completed ? 'Yes' : 'No' }}</TableCell>
-			</TableRow>
-		</TableBody>
-	</Table>
-</div>
+		  <div class="flex w-full max-w-sm items-center space-x-2">
+			<Input v-model="searchQuery" type="text" placeholder="Search tasks..." />
+			<Button :disabled="!searchQuery" @click="resetSearch" variant="outline" >
+				Reset
+			</Button>
+		  </div>
+		<Table class="mt-4">
+			<TableHeader>
+				<TableRow>
+					<TableHead>Task</TableHead>
+					<TableHead>Description</TableHead>
+					<TableHead>Completed</TableHead>
+				</TableRow>
+			</TableHeader>
+			<TableBody>
+				<TableRow v-if="filteredTasks.length === 0">
+					<TableCell colspan="3" class="text-center">No results</TableCell>
+				</TableRow>
+				<TableRow v-for="task in filteredTasks as Task[]" :key="task.id">
+					<TableCell>{{ task.name }}</TableCell>
+					<TableCell>{{ task.description }}</TableCell>
+					<TableCell>{{ task.completed ? 'Yes' : 'No' }}</TableCell>
+				</TableRow>
+			</TableBody>
+		</Table>
+	</div>
 </template>
 
 <style scoped lang="scss">
